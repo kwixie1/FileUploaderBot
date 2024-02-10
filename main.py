7272592246a116545a4ad14612c2bc43e7447b9b
Kwixie <kwixie1@gmail.com>
2024-02-10 20:37:06 +0200

    fileuploder package used instead of API call

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import nextcord
 from nextcord.ext import commands
-import aiohttp
 from nextcord.ui import Button, View
+import aiohttp
 import os
 from dotenv import load_dotenv
+import fileuploader
 
 load_dotenv()
 bot_token = os.getenv("TOKEN")
@@ -32,26 +33,15 @@ async def upload(ctx: commands.Context):
         await ctx.reply("Your file is too large! Max file size limit is 100 MB.")
         return
 
-    data = aiohttp.FormData()  # Create form-data body for request
-    data.add_field("file", await attachment.read(), filename=attachment.filename)  # Add file to the body
-
-    async with ctx.typing():  # Bot typing
-        async with aiohttp.ClientSession("https://fu.andcool.ru") as session:  # Create session with the API
-            async with session.post(f"/api/upload/private", data=data) as response:  # Send POST request
-                response_status = response.status
-                response = await response.json()
-
-    if response_status != 200:  # If server answered with error
-        errors = {  # List of known errors
-            500: "Internal server error",
-            502: "API didn't respond.",
-            522: "Server didn't respond"
-        }
-        embed_title = errors.get(response_status, "Unhandled error was occured")
-
+    try:
+        async with ctx.typing():
+            file = await fileuploader.upload(  # Upload file to a server
+                bytes=await attachment.read(),
+                filename=attachment.filename
+            )
+    except Exception as e:  # If server answered with error
         embed = nextcord.Embed(
-            title=embed_title, 
-            description=f"Status code: **{response_status}**",
+            title=e,
             color=nextcord.Color.from_rgb(170, 63, 68)
         )
         await ctx.reply(embed=embed)
@@ -59,14 +49,14 @@ async def upload(ctx: commands.Context):
 
     embed = nextcord.Embed(title="File successfully uploaded!", 
                            color=nextcord.Color.from_rgb(155, 181, 82))
-    embed.add_field(name="File link", value=response["file_url_full"])
-    embed.add_field(name="File name", value=response["user_filename"])
-    embed.set_image(url=response["file_url_full"])
+    embed.add_field(name="File link", value=file.file_url_full)
+    embed.add_field(name="File name", value=file.user_filename)
+    embed.set_image(url=file.file_url_full)
 
     button = Button(
         label="Delete file",
         style=nextcord.ButtonStyle.red, 
-        custom_id=f'del_{response["file_url"]}?key={response["key"]}_{ctx.author.id}'  # Store file info in button custom_id
+        custom_id=f'del_{file.file_url}_{file.key}_{ctx.author.id}'  # Store file info in button custom_id
     )
 
     view = View()
@@ -82,16 +72,17 @@ async def on_interaction(interaction: nextcord.Interaction):
     if interaction.data['custom_id'].startswith("del_"):  # If interacted component is delete button
         d_url = interaction.data['custom_id'].replace("del_", "").split("_")  # Get file and owner info
 
-        if str(interaction.user.id) != d_url[1]:  # If the interacted user is not owner
+        if str(interaction.user.id) != d_url[2]:  # If the interacted user is not owner
             await interaction.send(content="Hey, this button is not for you!", ephemeral=True)
             return
 
-        async with aiohttp.ClientSession("https://fu.andcool.ru") as session:  # Create session with the API
-            async with session.get(f"/api/delete/{d_url[0]}") as response:  # Create a GET request to delete a file
-                response_status = response.status
-    
-        if response_status != 200:  # If deletion not success
-            await interaction.response.send_message(f"Oops, something wents wrong. Status code is **{response_status}**")
+        try:  
+            await fileuploader.delete(  # Delete file from the server
+                file_url=d_url[0],
+                key=d_url[1]
+            )
+        except Exception as e:  # If server answered with error
+            await interaction.response.send_message(e)
             return
 
         del_embed = nextcord.Embed(
@@ -101,7 +92,7 @@ async def on_interaction(interaction: nextcord.Interaction):
             "has been deleted.",
             color=nextcord.Color.from_rgb(170, 63, 68)
         )
-                
+                    
         view = View()
         await interaction.message.edit(embed=del_embed, view=view)
 
