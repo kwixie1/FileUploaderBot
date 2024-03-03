e58aad2ab2345e61a19dcc35a55e07d608161552
Kwixie <kwixie1@gmail.com>
2024-03-03 19:24:54 +0200

    Added cooldown on upload command; bugfix

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import os
 from dotenv import load_dotenv
 import fileuploader
 import json
+import cooldowns
 
 load_dotenv()
 bot_token = os.getenv("TOKEN")
@@ -24,7 +25,7 @@ async def on_interaction(interaction: nextcord.Interaction):
     if interaction.type != nextcord.InteractionType.component:  # If interaction type is not component
         return
 
-    await interaction.message.edit(content="Wait a second...")  # Without it Discord can answer with an exception for long waiting
+    await interaction.message.edit(content="Wait a second...")  # Without it Discord can answer with an error for long waiting
     if interaction.data['custom_id'].startswith("del_"):  # If interacted component is delete button
         d_url = interaction.data['custom_id'].replace("del_", "").split("_")  # Get file and owner info
 
@@ -33,11 +34,10 @@ async def on_interaction(interaction: nextcord.Interaction):
             return
 
         try:
-            async with interaction.channel.typing():
-                await fileuploader.delete(  # Delete file from the server
-                    file_url=d_url[0],
-                    key=d_url[1]
-                )
+            await fileuploader.delete(  # Delete file from the server
+                file_url=d_url[0],
+                key=d_url[1]
+            )
         except Exception as e:  # If server answered with error
             await interaction.message.edit(content=None)
             await interaction.send(e, ephemeral=True)
@@ -73,12 +73,12 @@ async def help(interaction: nextcord.Interaction):
 
 
 @client.slash_command(description="Upload a file to the server")
+@cooldowns.cooldown(2, 60, bucket=cooldowns.SlashBucket.author)  # Command cooldown
 async def upload(interaction: nextcord.Interaction, attachment: nextcord.Attachment):
-    message = await interaction.send("Wait a second...")  # Save sended message for next editing
-    # Without it discord can response something like "the application did not respond"
+    await interaction.response.defer()
 
     if attachment.size >= 1024 * 1024 * 100:  # Chack attachment size
-        await message.edit("Your file is too large! Max file size limit is 100 MB.")
+        await interaction.followup.send("Your file is too large! Max file size limit is 100 MB.")
         return
 
     with open("data.json", "r") as f:
@@ -96,18 +96,17 @@ async def upload(interaction: nextcord.Interaction, attachment: nextcord.Attachm
                 json.dump(data, f, indent=4)  # Update the json file
 
         except Exception as e:
-            await message.edit(e)
+            await interaction.followup.send(e)
             return
         
     try:
-        async with interaction.channel.typing():
-            file = await fileuploader.upload(  # Upload file to the server
-                bytes=await attachment.read(),
-                filename=attachment.filename,
-                user=user
-            )
+        file = await fileuploader.upload(  # Upload file to the server
+            bytes=await attachment.read(),
+            filename=attachment.filename,
+            user=user
+        )
     except Exception as e:  # If server answered with error
-        await message.edit(e)
+        await interaction.followup.send(e)
         return
 
     embed = nextcord.Embed(title="File successfully uploaded!", 
@@ -126,21 +125,20 @@ async def upload(interaction: nextcord.Interaction, attachment: nextcord.Attachm
 
     view = View()
     view.add_item(button)
-    await message.edit(content=None, embed=embed, view=view)  # Send uploaded file info
+    await interaction.followup.send(embed=embed, view=view)  # Send uploaded file info
 
 
 @client.slash_command(description="log in the fu account")
 async def login(interaction: nextcord.Interaction, username: str, password: str):
-    message = await interaction.send("Wait a second...", ephemeral=True)
+    await interaction.response.defer(ephemeral=True)
 
     with open("data.json", "r") as f:
         data = json.load(f)
 
     try:
-        async with interaction.channel.typing():
-            user = await fileuploader.User.login(username, password, True)  # Log in the site
+        user = await fileuploader.User.login(username, password, True)  # Log in the site
     except Exception as e:
-        await message.edit(e)
+        await interaction.followup.send(e)
         return
 
     if str(interaction.user.id) in data:
@@ -153,18 +151,18 @@ async def login(interaction: nextcord.Interaction, username: str, password: str)
     with open("data.json", "w") as f:
         json.dump(data, f, indent=4)  # Update data.json
 
-    await message.edit(f"You are successfully logged in as `{user.username}`.")
+    await interaction.followup.send(f"You are successfully logged in as `{user.username}`.")
 
     
 @client.slash_command(description="log out from the fu account")
 async def logout(interaction: nextcord.Interaction):
-    message = await interaction.send("Wait a second...", ephemeral=True)
+    await interaction.response.defer(ephemeral=True)
 
     with open("data.json", "r") as f:
         data = json.load(f)  # Load data from .json file
 
     if str(interaction.user.id) not in data:  # If user not in data
-        await message.edit("You are already logged out!")
+        await interaction.followup.send("You are already logged out!")
         return
 
     userdata = data[str(interaction.user.id)]
@@ -175,7 +173,7 @@ async def logout(interaction: nextcord.Interaction):
     except fileuploader.exceptions.NotAuthorized:
         user = None
     except Exception as e:
-        await message.edit(e)
+        await interaction.followup.send(e)
         return
 
     del data[str(interaction.user.id)]  # Delete user data
@@ -184,27 +182,25 @@ async def logout(interaction: nextcord.Interaction):
 
     if user:
         try:
-            async with interaction.channel.typing():
                 await user.logout()  # Log out from the account
         except Exception as e:
-            await message.edit(e)
+            await interaction.followup.send(e)
             return
 
-    await message.edit("You are successfully logged out.")
+    await interaction.followup.send("You are successfully logged out.")
 
 
 @client.slash_command(description="register the new fu account")
 async def registration(interaction: nextcord.Interaction, username: str, password: str):
-    message = await interaction.send("Wait a second...", ephemeral=True)
+    await interaction.response.defer(ephemeral=True)
     
     with open("data.json", "r") as f:
         data = json.load(f)
 
     try:
-        async with interaction.channel.typing():
-            user = await fileuploader.User.register(username, password, True)  # Registrating a new account
+        user = await fileuploader.User.register(username, password, True)  # Registrating a new account
     except Exception as e:
-        await message.edit(e)
+        await interaction.followup.send(e)
         return
     
     if str(interaction.user.id) in data:
@@ -217,17 +213,20 @@ async def registration(interaction: nextcord.Interaction, username: str, passwor
     with open("data.json", "w") as f:
         json.dump(data, f, indent=4)  # Update the json file
 
-    await message.edit(f"You are successfully created an account and logged in as `{user.username}`.")
+    await interaction.followup.send(f"You are successfully created an account and logged in as `{user.username}`.")
 
 
 @client.event
-async def on_command_error(ctx: commands.Context, error):
-    if isinstance(error, commands.errors.CommandOnCooldown):
-        """Too many files handler"""
-        await ctx.reply(
+async def on_application_command_error(interaction: nextcord.Interaction, error):
+    error = getattr(error, "original", error)  # Get original error if avaible
+    if isinstance(error, cooldowns.CallableOnCooldown):  # If command on cooldown
+        await interaction.send(
             "Hey, slow down! You can upload only 2 files per 1 minute.\n" +
-            f"Try again at {int(error.retry_after)} sec"
+            f"Try again at {int(error.retry_after)} sec",
+            ephemeral=True
         )
+    else:
+        raise error
 
 
 if __name__ == "__main__":
